69259717dee7f86cf7ed0c817d1159f83065bcf9
Humar <hilmirsg@gmail.com>
2025-10-10 14:05:24 +0200

    More codin! yay + code not working

--- Assets/_Scripts/JobBoard/JobBoard.cs
+++ Assets/_Scripts/JobBoard/JobBoard.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CarePackage.Job
 {
     public class JobBoard : MonoBehaviour
     {
-        
+        [SerializeField] private Button[] jobButtons;
+
+        private void OnEnable()
+        {
+            foreach (var button in jobButtons)
+            {
+                button.onClick.AddListener((() => OnJobClicked(button.gameObject)));
+
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var button in jobButtons)
+            {
+                button.onClick.RemoveAllListeners();
+                Debug.Log("You have unclicked " + button.gameObject.name);
+            }
+        }
+
+        private void OnJobClicked(GameObject button)
+        {
+            Debug.Log("You have clicked " + button);
+        }
     }
 }
