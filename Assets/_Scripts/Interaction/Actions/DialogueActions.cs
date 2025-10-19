2be09ed98091a839e2f95e9d3e49e21d7a81c650
Marcela Estellés Alcaraz <160590533+marcestelles216@users.noreply.github.com>
2025-10-19 14:41:31 +0200

    Dialogue Family A Day 1
    Dialogue works without the interaction.

---
+++ Assets/_Scripts/Interaction/Actions/DialogueActions.cs
@@ -0,0 +1,28 @@
+﻿using CarePackage.Main;
+using System;
+using TMPro;
+using UnityEngine;
+using Yarn.Unity; 
+
+namespace CarePackage.Interaction
+{
+    [Serializable]
+    public class DialogueActions : InteractAction
+    {
+        [SerializeField] private string nodeName; 
+        [SerializeField] private DialogueRunner dialogueRunner;
+
+        public void PerformAction(PlayerState interactingPlayer, GameObject interactingObject)
+        {
+            if (dialogueRunner == null)
+            {
+                Debug.LogError("[DialogueAction] DialogueRunner no asignado en el inspector!");
+                return;
+            }
+
+            Debug.Log($"[DialogueAction] {interactingPlayer.name} inicia diálogo '{nodeName}' con {interactingObject.name}");
+
+            dialogueRunner.StartDialogue(nodeName);
+        }
+    }
+}
