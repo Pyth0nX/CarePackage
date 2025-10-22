8ea4274d9358fc9c1607442f1acc7f537d846914
Marcela Estellés Alcaraz <160590533+marcestelles216@users.noreply.github.com>
2025-10-22 15:36:10 +0200

    Some more dialogue

--- Assets/_Scripts/Interaction/Actions/DialogueActions.cs
+++ Assets/_Scripts/Interaction/Actions/DialogueActions.cs
@@ -8,23 +8,67 @@ namespace CarePackage.Interaction.Dialogue
 {
     [MovedFrom("CarePackage.Interaction")]
     [Serializable]
-    public class DialogueAction : InteractAction
+    public class DialogueAction : InteractAction, IActivatable
     {
         [SerializeField] private string nodeName;
         [SerializeField] private DialogueRunner dialogueRunner;
+
+        private PlayerController playerController;
 
         public void PerformAction(PlayerState interactingPlayer, GameObject interactingObject)
         {
             if (dialogueRunner == null)
             {
-                Debug.LogError("[DialogueAction] DialogueRunner no asignado en el inspector!");
-                return;
+                dialogueRunner = UnityEngine.Object.FindFirstObjectByType<DialogueRunner>();
+                if (dialogueRunner == null)
+                {
+                    Debug.LogError("[DialogueAction] DialogueRunner no asignado ni encontrado en la escena!");
+                    return;
+                }
             }
 
-            Debug.Log(
-                $"[DialogueAction] {interactingPlayer.name} inicia diálogo '{nodeName}' con {interactingObject.name}");
+            playerController = interactingPlayer.GetComponent<PlayerController>();
+
+            Debug.Log($"[DialogueAction] {interactingPlayer.name} inicia diálogo '{nodeName}' con {interactingObject.name}");
+
+            if (playerController == null)
+                return;
+            playerController.LockInput(true);
 
             dialogueRunner.StartDialogue(nodeName);
+        }
+
+        public void OnEnable()
+        {
+            if (dialogueRunner == null)
+            {
+                dialogueRunner = UnityEngine.Object.FindFirstObjectByType<DialogueRunner>();
+                if (dialogueRunner == null)
+                {
+                    Debug.LogError("[DialogueAction] DialogueRunner not in the scene");
+                    return;
+                }
+            }
+
+            dialogueRunner.onDialogueComplete.AddListener(OnDialogueComplete);
+        }
+
+        public void OnDisable()
+        {
+            if (dialogueRunner != null)
+            {
+                dialogueRunner.onDialogueComplete.RemoveListener(OnDialogueComplete);
+            }
+        }
+
+        private void OnDialogueComplete()
+        {
+            Debug.Log("[DialogueAction] Dialogue finished — input");
+
+            if (playerController == null)
+                return;
+            playerController.LockInput(false); 
+
         }
     }
 }
