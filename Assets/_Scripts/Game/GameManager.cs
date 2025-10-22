08026463555fca3a6eb9a81bd563edb6f513a3af
Marcela Estellés Alcaraz <160590533+marcestelles216@users.noreply.github.com>
2025-10-22 09:59:26 +0200

    Added gameplay state on Game Manager

--- Assets/_Scripts/Game/GameManager.cs
+++ Assets/_Scripts/Game/GameManager.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 namespace CarePackage.Main
 {
+    public enum GameState
+    {
+        Gameplay,
+        Dialogue,
+        Paused,
+    }
     public class GameManager : MonoBehaviour
     {
         public PlayerState Player;
         
         public static GameManager Instance;
-        
+        public GameState CurrentState { get; private set; } = GameState.Gameplay;
+
         private void Awake()
         {
             if (Instance == null) Instance = this;
         }
+
+        public void SetGameState(GameState newState)
+        {
+            CurrentState = newState;
+        }
+
+        public bool IsDialogueActive => CurrentState == GameState.Dialogue;
     }
 }
