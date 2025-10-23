211607de4acb77827abc78f9189aa670bc618562
Marcela Estellés Alcaraz <160590533+marcestelles216@users.noreply.github.com>
2025-10-23 14:08:41 +0200

    Dialogue fixed

--- Assets/_Scripts/Interaction/Actions/DialogueActions.cs
+++ Assets/_Scripts/Interaction/Actions/DialogueActions.cs
@@ -22,17 +22,26 @@ namespace CarePackage.Interaction.Dialogue
                 dialogueRunner = UnityEngine.Object.FindFirstObjectByType<DialogueRunner>();
                 if (dialogueRunner == null)
                 {
-                    Debug.LogError("[DialogueAction] DialogueRunner no asignado ni encontrado en la escena!");
+                    Debug.LogError("[DialogueAction] DialogueRunner not in the scene");
                     return;
                 }
             }
 
-            playerController = interactingPlayer.GetComponent<PlayerController>();
+            if (dialogueRunner.IsDialogueRunning)
+            {
+                Debug.LogWarning("[DialogueAction] Tried to start dialogue, but one is already running!");
+                return;
+            }
 
-            Debug.Log($"[DialogueAction] {interactingPlayer.name} inicia diálogo '{nodeName}' con {interactingObject.name}");
+            playerController = interactingPlayer.GetComponentInChildren<PlayerController>();
+
+            Debug.Log($"[DialogueAction] {interactingPlayer.name} initiates dialogue '{nodeName}' with {interactingObject.name}");
 
             if (playerController == null)
+            {
+                Debug.Log("Player Controller not found");
                 return;
+            }
             playerController.LockInput(true);
 
             dialogueRunner.StartDialogue(nodeName);
@@ -68,7 +77,6 @@ namespace CarePackage.Interaction.Dialogue
             if (playerController == null)
                 return;
             playerController.LockInput(false); 
-
         }
     }
 }
