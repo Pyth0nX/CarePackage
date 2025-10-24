3bf7897bf687797020fd88b39c209523aee21d02
Hilmir Snær Guðmundsson <hilmirsg@gmail.com>
2025-10-24 14:45:07 +0200

    Economy Manager

--- Assets/_Scripts/Game/EconomyManager.cs
+++ Assets/_Scripts/Game/EconomyManager.cs
@@ -1,22 +1,42 @@
+using System;
+using CarePackage.Main;
+using TMPro;
 using UnityEngine;
 
 public class EconomyManager : MonoBehaviour
 {
     [SerializeField] private int money;
-    [SerializeField] private int requiredMoney;
+    [SerializeField] private float requiredMoney;
+    [SerializeField] private TMP_Text moneyText;
     
-    public EconomyManager Instance;
+    public static EconomyManager Instance;
     
     private void Awake()
     {
         if (Instance == null) Instance = this;
     }
-    
+
+    private void OnEnable()
+    {
+        GameManager.Instance.OnDayStarted += OnDayStarted_Implementation;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.Instance.OnDayStarted -= OnDayStarted_Implementation;
+    }
+
     public int Bank => money;
-    public int GetRequiredMoney => requiredMoney;
+    public float GetRequiredMoney => requiredMoney;
     
     public void AddMoney(int amount)
     {
         money += amount;
+    }
+
+    public void OnDayStarted_Implementation()
+    {
+        requiredMoney *= 1.2f;
+        moneyText.text = money.ToString();
     }
 }
