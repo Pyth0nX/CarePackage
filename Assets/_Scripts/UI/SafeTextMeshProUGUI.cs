548cd3d69e95852b700e380138a019fb81f0e558
Marcela Estellés Alcaraz <160590533+marcestelles216@users.noreply.github.com>
2025-11-17 16:57:10 +0100

    Victor's help

---
+++ Assets/_Scripts/UI/SafeTextMeshProUGUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+
+
+internal class SafeTextMeshProUGUI : TextMeshProUGUI
+{
+    protected override void GenerateTextMesh()
+    {
+        try {base.GenerateTextMesh();}
+        catch(NullReferenceException) { }
+    }
+}
