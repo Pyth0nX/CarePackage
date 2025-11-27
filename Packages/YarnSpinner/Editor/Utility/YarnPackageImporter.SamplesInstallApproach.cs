b3b166009f655a6da67baa141250fdc8f5948d35
Marcela Estellés Alcaraz <160590533+marcestelles216@users.noreply.github.com>
2025-11-27 16:15:22 +0100

    Push b4 I keep working

--- Packages/YarnSpinner/Editor/Utility/YarnPackageImporter.SamplesInstallApproach.cs
+++
@@ -1,9 +0,0 @@
-namespace Yarn.Unity.Editor
-{
-    public static partial class YarnPackageImporter
-    {
-        // what install approach do we follow?
-        private static InstallApproach installApproach = InstallApproach.Manual;
-
-    }
-}
